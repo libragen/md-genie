10af4fe2eb0a7fa8099a2da961115b18452b7316
EricZhou <dejavuzhou@qq.com>
2018-07-19 22:25:20 +0800

    2018-07-19T22:25:15+08:00

--- main.go
+++ main.go
@@ -1,57 +1,52 @@
 package main
 
 import (
-	"fmt"
 	"os/exec"
 	_ "strconv"
 	"time"
 
 	"github.com/mojocn/movie-board/util"
+	"fmt"
 )
 
 
 
-const movie_url = "http://m.maoyan.com/ajax/movieOnInfoList"
 
-func main()  {
-	util.ParseMarkdown()
-}
-func first() {
+
+func main() {
 	//loop
-	for {
+	//for {
 		util.FetchMaoyanApi()
 		util.ParseMarkdown()
-		commitMsg := time.Now().Format(time.RFC3339)
-		gitPull()
-		gitAddAll()
-		gitCommit(commitMsg)
-		gitPush()
 
-		time.Sleep(3 * time.Hour)
-	}
-}
-
-func dateString() string {
-	y, m, d := time.Now().Date()
-	mStr := fmt.Sprintf("%d", m)
-	dStr := fmt.Sprintf("%d", d)
-	if m < 10 {
-		mStr = fmt.Sprintf("0%d", m)
-	}
-	if d < 10 {
-		dStr = fmt.Sprintf("0%d", d)
-	}
-	return fmt.Sprintf("%d-%s-%s", y, mStr, dStr)
-
+		runGitCmds()
+	//	time.Sleep(3 * time.Hour)
+	//}
 }
 
 
-func gitPull() {
+func runGitCmds(){
+	commitMsg := time.Now().Format(time.RFC3339)
+
+	cmds := [][]string{
+		[]string{"stash"},
+		[]string{"pull","origin","master"},
+		[]string{"stash","apply"},
+		[]string{"add","archives/*"},
+		[]string{"commit","-am",commitMsg},
+		[]string{"push","origin","master"},
+	}
+	for _,arguments := range cmds {
+		gitCommand(arguments...)
+	}
+}
+
+
+
+
+func gitCommand(args ...string) {
 	app := "git"
-	arg0 := "pull"
-	arg1 := "origin"
-	arg2 := "master"
-	cmd := exec.Command(app, arg0, arg1, arg2)
+	cmd := exec.Command(app,args ...)
 	out, err := cmd.Output()
 
 	if err != nil {
@@ -59,51 +54,5 @@ func gitPull() {
 		return
 	}
 
-	print(string(out))
-}
-
-func gitAddAll() {
-	app := "git"
-	arg0 := "add"
-	arg1 := "."
-	cmd := exec.Command(app, arg0, arg1)
-	out, err := cmd.Output()
-
-	if err != nil {
-		println(err.Error())
-		return
-	}
-
-	print(string(out))
-}
-
-func gitCommit(date string) {
-	app := "git"
-	arg0 := "commit"
-	arg1 := "-am"
-	arg2 := date
-	cmd := exec.Command(app, arg0, arg1, arg2)
-	out, err := cmd.Output()
-
-	if err != nil {
-		println(err.Error())
-		return
-	}
-
-	print(string(out))
-}
-func gitPush() {
-	app := "git"
-	arg0 := "push"
-	arg1 := "origin"
-	arg2 := "master"
-	cmd := exec.Command(app, arg0, arg1, arg2)
-	out, err := cmd.Output()
-
-	if err != nil {
-		println(err.Error())
-		return
-	}
-
-	print(string(out))
+	fmt.Print(string(out))
 }
