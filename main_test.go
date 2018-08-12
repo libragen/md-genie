69e53a6052c9d32fb2b928b8a5e7936a67de722a
EricZhou <dejavuzhou@qq.com>
2018-08-12 14:00:26 +0800

    2018-08-12T14:00:18+08:00

---
+++ main_test.go
@@ -0,0 +1,39 @@
+package main
+import (
+
+	"github.com/dejavuzhou/md-genie/util"
+	"time"
+	"testing"
+)
+
+/*
+	有道智云
+	文档页面  http://ai.youdao.com/docs/doc-trans-api.s#p01
+*/
+func Test_main(t *testing.T) {
+
+	if err := util.SpiderHackNews();err != nil {
+		util.SendMsgToEmail("spider hack news error", err.Error(), "erikchau@me.com")
+	}
+	if err := util.ParseMarkdownHacknews();err != nil {
+		util.SendMsgToEmail("pasrse hack news markdown error", err.Error(), "erikchau@me.com")
+	}
+
+	if err := util.FetchMaoyanApi();err != nil {
+		util.SendMsgToEmail("fetch maoyan api error", err.Error(), "erikchau@me.com")
+	}
+	if err := util.ParseMaoyanMarkdown();err != nil {
+		util.SendMsgToEmail("parse maoyan movie markdown error", err.Error(), "erikchau@me.com")
+	}
+
+	util.ParseReadmeMarkdown()
+	mailTitle, gitlog := runGitCmds()
+
+	if err,mailBody := util.ParseEmailContent(gitlog);err == nil {
+		util.SendMsgToEmail(mailTitle, mailBody, "erikchau@me.com")
+	}else {
+		util.SendMsgToEmail("parse email content hmtl error", err.Error(), "erikchau@me.com")
+	}
+	time.Sleep(6 * time.Hour)
+}
+
